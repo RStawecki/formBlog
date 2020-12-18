00621db57ada25e89ee0e720f735c13df6cfb814
Rafał Stawecki <rafal.stawecki@gmail.com>
2020-12-18 22:48:45 +0100

    styling css flexbox layout

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    return render(request, 'blog/detail.html')
+    return render(request, 'blog/detail.html', {'post': post})
 
 def create_form(request):
     
